033318b0944df2a44e8b762b9e71d337084fbd53
alekspribysh <barselona1985>
2015-09-01 07:43:04 -0700

    Initial commit

--- src/main/java/com/filter/filterLogin.java
+++ src/main/java/com/filter/filterLogin.java
@@ -11,23 +11,28 @@ import java.io.IOException;
 /**
  * Created by alekspribysh on 8/31/15.
  */
-@WebServlet(urlPatterns = {"/*"})
+//@WebServlet(urlPatterns = {"/*"})
 public class filterLogin implements javax.servlet.Filter {
 
+    FilterConfig filterConfig = null;
 
     public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
 
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         ServletContext context = request.getServletContext();
-        String name = (String) context.getAttribute("name");
+       String name = (String) context.getAttribute("name");
 
-        if (name != null){
+
+        if (name == null){
             chain.doFilter(request,response);
 
         }
+
+        else ();
     }
 
     public void destroy() {
